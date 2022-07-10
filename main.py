9ed32e124143f9e1403cab25359984728890cb90
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-07-10 10:45:18 -0400

    remove unused imports

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+
 
 url="http://orteil.dashnet.org/experiments/cookie/"
 
